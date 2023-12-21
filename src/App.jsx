103dd73781d9e17eb3729ff5d9b23050fdb12908
tywonmosley <146139910+tywonmosley@users.noreply.github.com>
2023-12-21 16:12:58 -0500

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+import { puppyList } from "./data.js";
+
 
 function App() {
+  const [state, setState] = useState(0);
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log(puppyList);
+
   return (
     <>
       <div>
-
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}> {puppy.name} </p>;
+        })}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
